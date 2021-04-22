85b1751a0a3da41b50942678fdb8e9d738fd2ceb
Jensen Hwa <30474284+jensenhwa@users.noreply.github.com>
2021-04-22 17:16:04 -0400

    Visualization improvements and refactoring

--- main.py
+++ main.py
@@ -1,46 +1,64 @@
 import cv2
-from depth import transform
+from depth.transform import DepthTracker
 import numpy as np
 import os
-import tracking
+from tracking import ObjectTracker
 
 
 if __name__ == "__main__":
-  video_name = 'PXL_20210418_183745902'
-  input_video = os.path.join('depth', 'input', video_name + '.mp4')
-  cap = cv2.VideoCapture(input_video)
-  fps = cap.get(cv2.CAP_PROP_FPS)
-  currentFrame = 0
-  objectTrackerFramerate = 3
-  scaleFactor = 0.25
-  tracker_person = tracking.CSRT_Tracker()
-  tracker_landmark = tracking.CSRT_Tracker()
+    video_name = 'PXL_20210418_183745902'
+    input_video = os.path.join('depth', 'input', video_name + '.mp4')
+    cap = cv2.VideoCapture(input_video)
+    fps = round(cap.get(cv2.CAP_PROP_FPS))
+    objectTrackerFPS = 10
+    scaleFactor = 0.25
 
-  intrinsic_matrix = np.loadtxt(os.path.join('camera_calibration', 'intrinsics.cfg'))
-  depth_tracker = transform.DepthTracker(intrinsic_matrix, scale_factor=scaleFactor)
-  dist_traveled = 0
-  img_name = 0
+    intrinsic_matrix = np.loadtxt(os.path.join('camera_calibration', 'intrinsics.cfg'))
+    depth_tracker = DepthTracker(intrinsic_matrix, scale_factor=scaleFactor)
+    dist_traveled = 0
+    seconds_elapsed = 0
 
-  while cap.isOpened():
+    if not cap.isOpened():
+        print("Cannot open video")
+        exit()
     ret, frame = cap.read()
-    if ret:
-      img = cv2.resize(frame, (0, 0), fx=scaleFactor, fy=scaleFactor)
-      x1, y1 = tracker_person.track(img)
-      x2, y2 = tracker_landmark.track(img)
-      currentFrame += objectTrackerFramerate
-      cap.set(1, currentFrame)
-      if currentFrame >= img_name * fps:
-        currentFrame = img_name * fps
-        cap.set(1, currentFrame)
-        if x1 is not None and x2 is not None:
-          coords = depth_tracker.get_coordinates(str(img_name), x1, y1, x2, y2, video_name)
-          dist = depth_tracker.compute_dist(coords)
-          print(f"in image {img_name} richard is at {coords}")
-          print('dist from prev frame', dist)
-          dist_traveled += dist
-        img_name += 1
-    else:
-      cap.release()
-      break
-  cv2.destroyAllWindows()
-  print(dist_traveled)
+    img = cv2.resize(frame, (0, 0), fx=scaleFactor, fy=scaleFactor)
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        exit()
+    print("Select bounding box for person, then hit enter")
+    tracker_person = ObjectTracker(img, "Richard Guan, 20", color=(0, 255, 0))
+    print("Select bounding box for landmark, then hit enter")
+    tracker_landmark = ObjectTracker(img, "Landmark", color=(0, 0, 255))
+
+    distances = []
+    currentFrame = 0
+    while cap.isOpened():
+        # Run object tracker every few frames
+        currentFrame += fps / objectTrackerFPS
+        cap.set(cv2.CAP_PROP_POS_FRAMES, currentFrame)
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        img = cv2.resize(frame, (0, 0), fx=scaleFactor, fy=scaleFactor)
+        x1, y1 = tracker_person.track(img)
+        x2, y2 = tracker_landmark.track(img)
+        # Compute movement every second (based on fps)
+        if currentFrame % fps == 0:
+            coords = depth_tracker.get_coordinates(str(seconds_elapsed), x1, y1, x2, y2, video_name)
+            dist = depth_tracker.compute_dist(coords)
+            print(f"at second {seconds_elapsed} richard is at {coords}")
+            print('dist from prev frame', dist)
+            dist_traveled += dist
+            distances.append((x1, y1, dist_traveled))
+            seconds_elapsed += 1
+
+        for (x1, y1, dist_traveled) in distances:
+            cv2.putText(img, f"{dist_traveled:.1f}m", (x1, y1 - 5), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+        cv2.imshow('BallerStats', img)
+        cv2.waitKey(1)
+
+    cap.release()
+    cv2.destroyAllWindows()
+    print(dist_traveled)
--- tracking.py
+++ tracking.py
@@ -1,36 +1,41 @@
 import cv2
+import numpy as np
 
 
-class CSRT_Tracker():
-  def __init__(self):
-    self.tracker = cv2.TrackerCSRT_create()
-    self.bounding_box = None
+class ObjectTracker:
+    def __init__(self, frame, name, color=(0, 255, 0)):
+        self.tracker = cv2.TrackerCSRT_create()
+        bounding_box = cv2.selectROI(f'Select bounding box for {name}', frame, fromCenter=False, showCrosshair=True)
+        cv2.destroyWindow(f'Select bounding box for {name}')
+        self.tracker.init(frame, bounding_box)
+        self.name = name
+        self.color = color
+        self.locations = np.array(
+            [[[bounding_box[0] + round(bounding_box[2] / 2), bounding_box[1] + round(bounding_box[3] / 2)]]])
 
-  def track(self, frame):
-    x, y, w, h = None, None, None, None
+    def track(self, frame):
+        (success, box) = self.tracker.update(frame)
+        if not success:
+            raise RuntimeError("Object has been lost!")
+        (x, y, w, h) = box
+        center = (x + w // 2, y + h // 2)
+        self.locations = np.append(self.locations, [[center]], axis=1)
+        darker_color = tuple(np.maximum((0, 0, 0), np.subtract(self.color, (70, 70, 70))).tolist())
+        cv2.polylines(frame, self.locations, False, darker_color, 2)
+        cv2.putText(frame, self.name, (x, y - 5), cv2.FONT_HERSHEY_DUPLEX, 0.7, self.color)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), self.color, 2)
+        return center
 
-    if self.bounding_box is not None:
-      (success, box) = self.tracker.update(frame)
-      if success:
-        (x, y, w, h) = [int(v) for v in box]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-      cv2.imshow('frame', frame)
-      cv2.waitKey(1)
-    else:
-      self.bounding_box = cv2.selectROI('frame', frame, fromCenter=False, showCrosshair=True)
-      self.tracker.init(frame, self.bounding_box)
-      x, y, w, h = self.bounding_box
-    return x + w // 2,  y + h // 2
 
 def track_bb(input_video):
-  video_stream = cv2.VideoCapture(input_video)
-  tracker = CSRT_Tracker()
-  while True:
-    frame = video_stream.read()[1]
-    tracker.track(frame)
+    video_stream = cv2.VideoCapture(input_video)
+    tracker = ObjectTracker(video_stream.read()[1])
+    while True:
+        frame = video_stream.read()[1]
+        tracker.track(frame)
 
 
 if __name__ == "__main__":
-  # create main window
-  track_bb("./depth/input/PXL_20210418_183745902.mp4")
-  cv2.destroyAllWindows()
+    # create main window
+    track_bb("./depth/input/PXL_20210418_183745902.mp4")
+    cv2.destroyAllWindows()
